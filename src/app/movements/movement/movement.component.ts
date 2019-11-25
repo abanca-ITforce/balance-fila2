8ec17597e5ed2620c1e62014775e1827282509df
FatimaPrego <fatimaprego@gmail.com>
2019-11-25 19:59:47 +0100

    v5.4 card movement

--- src/app/movements/movement/movement.component.ts
+++ src/app/movements/movement/movement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'ab-movement',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovementComponent implements OnInit {
 
-  constructor() { }
+  movementTitle: string;
+  constructor(activatedRoute: ActivatedRoute) {
+    this.movementTitle = activatedRoute.snapshot.params.id;
+   }
 
   ngOnInit() {
   }
